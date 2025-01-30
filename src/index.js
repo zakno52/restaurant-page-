a9a71accaf76d46d80e23d3225a6e4860d71ba6a
zakno52 <101570337+zakno52@users.noreply.github.com>
2025-01-30 02:22:15 +0100

    Implement JavaScript

--- src/index.js
+++ src/index.js
@@ -1,2 +1,26 @@
 import "./styles.css";
-console.log("works");
+
+const pages = (function name(params) {
+  // variables
+  const navButton = document.querySelectorAll(".center");
+  const output = document.getElementById("content");
+  // variables-creatElements
+  const heroLogo = document.createElement("h1");
+  heroLogo.innerHTML = "ELYSIAN";
+  // Events
+  navButton.forEach((button, index) => {
+    button.addEventListener("click", () => showPage(index));
+  });
+
+  //handlers
+  function showPage(index) {
+    if (index === 0) {
+      heroLogo.classList.add("logo");
+      output.append(heroLogo);
+    }
+    if (index === 1) {
+    }
+    if (index === 2) {
+    }
+  }
+})();
